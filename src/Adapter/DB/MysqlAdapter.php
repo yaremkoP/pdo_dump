3993474090f7af23f06de2e1d84c1d4ccee436c8
Pavlo Yaremko <Pavlo_Yaremko@epam.com>
2020-07-19 16:42:04 +0300

    Added host/port handling for mysql

--- src/Adapter/DB/MysqlAdapter.php
+++ src/Adapter/DB/MysqlAdapter.php
@@ -7,6 +7,11 @@ use PDO;
 class MysqlAdapter extends BaseAdapter implements PDOInterface
 {
     /**
+     * @var int Default Port
+     */
+    protected int $default_port = 3306;
+
+    /**
      * MysqlConnector constructor.
      *
      * @param string $host
@@ -17,7 +22,7 @@ class MysqlAdapter extends BaseAdapter implements PDOInterface
     public function __construct(string $host, string $database, string $username, string $password)
     {
         $this->driver  = 'mysql';
-        $this->port    = 3306;
+        $this->port    = $this->fetchPort($host);
         $this->options = [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -26,5 +31,24 @@ class MysqlAdapter extends BaseAdapter implements PDOInterface
         ];
 
         parent::__construct($host, $database, $username, $password);
+    }
+
+    /**
+     * Determine port and host
+     *
+     * @param string $host Host string, could contain port
+     *
+     * @return int
+     */
+    private function fetchPort(string &$host)
+    {
+        $port = $this->default_port;
+        if (strpos($host, ':')) {
+            $parts = explode(':', $host);
+            $host  = $parts[0];
+            $port  = (int)$parts[1];
+        }
+
+        return $port;
     }
 }
